8ffb8e80a8c7bedadb8200f777d2540812b056a3
michaeldai1006 <dtc74110@gmail.com>
2020-03-26 15:22:56 -0400

    c6-db-core clean up

--- Data Storage/c6-db-core/app.js
+++ Data Storage/c6-db-core/app.js
@@ -6,14 +6,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,12 +16,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).json({
+    status: false,
+    message: 'ROUTE NOT FOUND'
+  });
 });
 
 // error handler
@@ -35,9 +32,12 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  console.log(err);
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    status: false,
+    message: err.message || err
+  });
 });
 
 module.exports = app;
