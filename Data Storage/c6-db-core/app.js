5761235be32509963156a0bd5c11527c6df3db1e
michaeldai1006 <dtc74110@gmail.com>
2020-03-26 15:26:09 -0400

    tweet route

--- Data Storage/c6-db-core/app.js
+++ Data Storage/c6-db-core/app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var usersRouter = require('./routes/users');
+const tweetRoute = require('./routes/tweet.js');
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/users', usersRouter);
+app.use('/tweet', tweetRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
