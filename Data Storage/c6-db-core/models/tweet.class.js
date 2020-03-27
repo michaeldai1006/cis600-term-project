4e0b255203263fb66c15ba3f3d524173e03d2c1f
michaeldai1006 <dtc74110@gmail.com>
2020-03-27 10:37:05 -0400

    C6Tweet

---
+++ Data Storage/c6-db-core/models/tweet.class.js
@@ -0,0 +1,56 @@
+const db = require('../services/db.conn');
+const md5 = require('md5');
+
+class C6Tweet {
+    constructor(tweet_token, tweet_id) {
+        this.tweet_token = tweet_token;
+        this.tweet_id = tweet_id;
+    }
+
+    async registerTweet(tweet_info, user_id, place_id) {
+        // Verify tweet info
+        if (!tweet_info) throw new Error('MISSING TWEET INFO');
+        if (!user_id) throw new Error('MISSING USER ID INFO');
+
+        try {
+            // Extract info from tweet info
+            const { tw_tweet_id, text, latitude, longitude, lang, created_at } = tweet_info;
+
+            // Insert query
+            const insertQuery = `
+                INSERT INTO c6_tweet (tw_tweet_id, text, latitude, longitude, lang, created_at, user_id, place_id, cdate, udate, status)
+                VALUSE (${tw_tweet_id}, '${text}', ${latitude}, ${longitude}, '${lang}', ${created_at}, ${user_id}, ${place_id}, NOW(), NOW(), 0)
+            `;
+
+            // Perform insert query
+            const insertQueryRes = await db.performQuery(insertQuery);
+
+            // Insert ID
+            const insert_id = insertQueryRes['insertId'];
+            if (!insert_id) throw new Error('INSERT TWEET RECORD FAILED');
+            this.tweet_id = insert_id;
+
+
+            // Tweet token
+            const tweet_token = `TWT-${'' + insert_id + tw_tweet_id + 'dsk'}`;
+            this.tweet_token = tweet_token;
+
+            // Update query: tweet token, udate, status
+            const updateQuery = `
+                UPDATE c6_tweet 
+                SET tweet_token = '${tweet_token}', udate = NOW(), status = 1
+                WHERE id = ${insert_id}
+            `;
+
+            // Perform update query
+            await db.performQuery(updateQuery);
+
+            // Tweet token result
+            return tweet_token;
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = C6Tweet;
---
+++ Data Storage/c6-db-core/services/db.conn.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.C6_DB_HOST,
+    user: process.env.C6_DB_USER,
+    password: process.env.C6_DB_PASS,
+    database: 'c6_core'
+});
+
+const performQuery = (query) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            } else {
+                conn.query(query, (err, res) => {
+                    if (err) {
+                        if (err.errno === 1213) performQuery(query).then(result => resolve(result)).catch(t_err => reject(t_err)); else reject(err);
+                    } else resolve(res);
+                    conn.release();
+                });
+            }
+        });
+    });
+};
+
+module.exports.performQuery = performQuery;
