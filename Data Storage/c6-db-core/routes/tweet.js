33aaa9022ae6ed75a21ba02aff6d1ad53ed7642a
michaeldai1006 <dtc74110@gmail.com>
2020-04-06 01:29:02 -0400

    Tweet route

--- Data Storage/c6-db-core/routes/tweet.js
+++ Data Storage/c6-db-core/routes/tweet.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const C6TweetAction = require('../actions/tweet.action');
 
 // Register new tweet data
 router.post('/detail', async (req, res, next) => {
-  res.send('lol');
+  try {
+    // Tweet record list
+    const { record_list } = req.body;
+    const payload = await C6TweetAction.registerTweets(record_list);
+
+    // Response
+    res.json({
+      status: true,
+      message: 'REGISTER TWEETS SUCCEED',
+      payload
+      });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
